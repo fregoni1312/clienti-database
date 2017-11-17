e184c7608cb641db2df301729bf504bbaf91740f
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-17 21:36:12 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,15 +1,10 @@
 <?php
    $file = ("conta.txt");
    $visite = file($file);
-   echo "visite vale: $visite[0]                      ";
    $ciao=$visite[0]+1;
-   echo "ciao vale: $ciao                             ";
    $fp = fopen($file , "w");
-   ////fputs($file , "$ciao");
    file_put_contents($file, $ciao); // solo PHP 5!!!
    fclose($fp);
- 
-    //echo $ciao;
+
    echo "Hai effetuato  $ciao visite a questo sito.";
-   ////echo "    Hai effetuato  $visite[0] visite a questo sito.";
 ?>
