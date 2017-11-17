b91412256e88746d01890b75240f24b659fbf5f9
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-17 20:48:00 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@ if (!$cook)$contador ++; // // se non esiste il cookie incremento i visitatori
 // Creo archivio contatore. Será con la 1ª visita
 touch($archivo);
 chmod($archivo, 0755);
-$conta= 10; // Valor por defecto si no existe fichero de visitas
+$conta= 1; // Valor por defecto si no existe fichero de visitas
 }
 
 // Grabamos cookie de 1 hora de duracion
