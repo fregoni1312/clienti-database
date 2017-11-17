10546fcd88c339079ef47e27bab5e253a103e2ce
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-17 21:27:34 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,8 +5,8 @@
    $fp = fopen($file , "w");
    fputs($file , "$ciao");
   //// file_put_contents($file, $ciao); // solo PHP 5!!!
-   //fclose($fp);
-  echo "ciao";
-    //echo $ciao;
+   fclose($fp);
+ 
+    echo $ciao;
    ////echo "    Hai effetuato  $visite[0] visite a questo sito.";
 ?>
