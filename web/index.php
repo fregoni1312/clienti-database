31b328641cc66829eb824602e2f443e2f2064526
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-17 21:22:07 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,9 +2,9 @@
    $file = ("conta.txt");
    $visite = file($file);
    $ciao=$visite+1;
-   //$fp = fopen($file , "w");
-   //fputs($file , "$visite[0]");
-   file_put_contents($file, $visitie[0]); // solo PHP 5!!!
+   $fp = fopen($file , "w");
+   fputs($file , "$visite[0]");
+  // file_put_contents($file, $visitie[0]); // solo PHP 5!!!
    fclose($fp);
   
     echo $ciao;
