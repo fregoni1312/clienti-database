79e8318fec6009a402e3eaed53791db0bef987ad
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-17 21:08:40 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,8 +5,8 @@
    $fp = fopen($file , "w");
    fputs($fp , "$visite[0]");
    fclose($fp);
-   $ciao=0;
+   $ciao=5;
    $ciao++;
 echo $ciao;
-   echo "Hai effetuato  $visite[0] visite a questo sito.";
+   echo "    Hai effetuato  $visite[0] visite a questo sito.";
 ?>
