24ac75392a2f2a7135f7181724e53be861c8cee5
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-12-10 19:48:12 +0100

    Delete index.php

--- web/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $file = ("conta.txt");
-   $visite = file($file);
-   $ciao=$visite[0]+1;
-   $fp = fopen($file , "w");
-   file_put_contents($file, $ciao); // solo PHP 5!!!
-   fclose($fp);
-   echo "Hai effetuato  $ciao visite a questo sito.";
-?>
